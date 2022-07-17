ca2ba788eb9526be76d8e1566b8b9d9975e5fcbb
Anilkumarcdac <aiml.anilkumar@gmail.com>
2022-07-17 20:58:40 +0530

    new updates

--- add.py
+++ add.py
@@ -1,9 +1,12 @@
 a=20
 b=40
 c=a+b
-print(c)
+print("The addition result is",c)
 
 d=10
 e=10
 f=d*e
-print(f)
+print("The multiplication result is",f)
+
+g=c+f
+print("The addition of all operation result is",g)
---
+++ cal.py
@@ -0,0 +1,13 @@
+a=60
+b=40
+c=a-b
+print("The subtaction result is",c)
+
+d=a/b
+print("The division result is",d)
+
+e=a+b
+print("The addition result is",e)
+
+f=a*b
+print("The multiplication result is",f)
