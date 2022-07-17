44c20a02b8ec2e581d3b8be63094f6a78a609319
Anilkumarcdac <aiml.anilkumar@gmail.com>
2022-07-17 19:28:28 +0530

    change done

---
+++ add.py
@@ -0,0 +1,9 @@
+a=20
+b=40
+c=a+b
+print(c)
+
+d=10
+e=10
+f=d*e
+print(f)
---
+++ sub.py
@@ -0,0 +1,9 @@
+a=60
+b=40
+c=a-b
+print("The subtaction result is",c)
+
+d=10
+e=10
+f=d/e
+print("The division result is",f)
